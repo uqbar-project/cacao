98d50f791f8539602761bc2d2bd395a130e70995
Nicolas <Nicolas Scarcella>
2014-10-14 01:40:23 -0300

    Updated to scala 2.11.2 and better immutable scalable test

--- src/main/scala/org/uqbar/cacao/dimensions/Scalable.scala
+++ src/main/scala/org/uqbar/cacao/dimensions/Scalable.scala
@@ -33,35 +33,52 @@ trait Scalable[T <: Scalable[T]] { self: T =>
 	def scaleToFit(width: Double, height: Double): Unit = scaleBy(min(width / size.x, height / size.y))
 
 	def scaleToCover(size: Vector): Unit = scaleToCover(size.x, size.y)
-	def scaleToCover(width: Double, height: Double): Unit = scaleBy(max(width / size.x, height / size.y))
+	def scaleToCover(width: Double, height: Double): Unit = if (width * height == 0) scaleBy(0) else scaleBy(max(width / size.x, height / size.y))
 
 	//*********************************************************************************************
 	// IMMUTABLE
 	//*********************************************************************************************
 
-	// If x == 0 || y == 0 should raise error
 	def scaledBy(x: Double = 1, y: Double = 1): T
 
 	def scaledBy(scaleRatios: Vector): T = scaledBy(scaleRatios.x, scaleRatios.y)
 	def scaledBy(ratio: Double): T = scaledBy(ratio, ratio)
 
 	def scaledTo(targetSize: Vector): T = scaledTo(targetSize.x, targetSize.y)
-	def scaledTo(width: Double = NaN, height: Double = NaN): T =
-		if (width.isNaN && height.isNaN) throw new InvalidParameterException
+	def scaledTo(width: Double = NaN, height: Double = NaN): T = {
+		require(!width.isNaN || !height.isNaN, "At least one argument must be supplied.")
+		require(size.x != 0 || width == 0)
+		require(size.y != 0 || height == 0)
+
+		if (size.x == 0 || size.y == 0) scaledBy(0)
 		else if (width.isNaN) scaledBy(height / size.y)
 		else if (height.isNaN) scaledBy(width / size.x)
 		else scaledBy(width / size.x, height / size.y)
+	}
 
-	def scaledHorizontallyTo(width: Double): T = scaledBy(x = width / size.x)
-	def scaledVerticallyTo(height: Double): T = scaledBy(y = height / size.y)
+	def scaledHorizontallyTo(width: Double): T = {
+		require(size.x != 0 || width == 0)
+
+		if (size.x == 0) scaledBy(x = 0) else scaledBy(x = width / size.x)
+	}
+	def scaledVerticallyTo(height: Double): T = {
+		require(size.y != 0 || height == 0)
+
+		if (size.y == 0) scaledBy(y = 0) else scaledBy(y = height / size.y)
+	}
 
 	def scaledToFit(size: Vector): T = scaledToFit(size.x, size.y)
-	def scaledToFit(width: Double, height: Double): T = if (width == 0 || height == 0)
-		throw new InvalidParameterException
-	else scaledBy(min(width / size.x, height / size.y))
+	def scaledToFit(width: Double, height: Double): T = {
+		require(size.x != 0 || width == 0)
+		require(size.y != 0 || height == 0)
+		if (size.x * size.y == 0) scaledBy(0) else scaledBy(min(width / size.x, height / size.y))
+	}
 
 	def scaledToCover(size: Vector): T = scaledToCover(size.x, size.y)
-	def scaledToCover(width: Double, height: Double): T = if (width == 0 || height == 0)
-		throw new InvalidParameterException
-	else scaledBy(max(width / size.x, height / size.y))
+	def scaledToCover(width: Double, height: Double): T = {
+		require(size.x != 0 || width == 0)
+		require(size.y != 0 || height == 0)
+
+		if (size.x * size.y == 0 || width * height == 0) scaledBy(0) else scaledBy(max(width / size.x, height / size.y))
+	}
 }
--- src/test/scala/org/uqbar/cacao/dimensions/ScalableTest.scala
+++ src/test/scala/org/uqbar/cacao/dimensions/ScalableTest.scala
@@ -4,7 +4,6 @@ import org.scalatest.FreeSpec
 import org.scalatest.Matchers
 import org.scalatest.BeforeAndAfter
 import org.uqbar.math.vectors._
-import java.security.InvalidParameterException
 
 class ScalableTest extends FreeSpec with Matchers with BeforeAndAfter {
 
@@ -19,14 +18,14 @@ class ScalableTest extends FreeSpec with Matchers with BeforeAndAfter {
 			"a ratio in (1, ∞] should grow proportionally" in { s.scaledBy(2).size should be (s.size * 2) }
 			"a ratio = 1 should stay the same" in { s.scaledBy(1).size should be (s.size) }
 			"a ratio in (0, 1) should shrink proportionally" in { s.scaledBy(0.5).size should be (s.size / 2) }
-			"a ratio = 0 should raise an exception" in { an[InvalidParameterException] should be thrownBy s.scaledBy(0) }
+			"a ratio = 0 should shrink to size zero" in { s.scaledBy(0).size should be (Origin) }
 			"a ratio in (-1, 0) should shrink proportionally and change it's size's sign" in { s.scaledBy(-0.5).size should be (-s.size / 2) }
 			"a ratio = -1 should change it's size's sign" in { s.scaledBy(-1).size should be (-s.size) }
 			"a ratio in [-∞,-1) should grow proportionally and change it's size's sign" in { s.scaledBy(-1).size should be (-s.size) }
 		}
 
-		"scaled" in {
-			val ratios = List(-2, -1, -0.5, -Double.MinPositiveValue, Double.MinPositiveValue, 0.5, 1, 2)
+		"scaledBy" in {
+			val ratios = List(Double.MinValue, -2, -1, -0.5, -Double.MinPositiveValue, 0, Double.MinPositiveValue, 0.5, 1, 2, Double.MaxValue)
 
 			for (n <- ratios) {
 				s.scaledBy(n, n).size should be (s.scaledBy(n).size)
@@ -34,55 +33,108 @@ class ScalableTest extends FreeSpec with Matchers with BeforeAndAfter {
 				s.scaledBy(y = n).size should be (s.scaledBy(1, n).size)
 				s.scaledBy(x = n).scaledBy(y = n).size should be (s.scaledBy(n, n).size)
 				s.scaledBy(x = n).scaledBy(y = n).size should be (s.scaledBy(n).size)
+
+				S(0, 0).scaledBy(n, n).size should be (Origin)
+				S(0, 0).scaledBy(x = n).size should be (Origin)
+				S(0, 0).scaledBy(y = n).size should be (Origin)
 			}
+		}
 
-			an[InvalidParameterException] should be thrownBy s.scaledTo(1, 0)
-			an[InvalidParameterException] should be thrownBy s.scaledTo(0, 1)
-			an[InvalidParameterException] should be thrownBy s.scaledTo(0, 0)
-			an[InvalidParameterException] should be thrownBy s.scaledTo(Origin)
-			an[InvalidParameterException] should be thrownBy s.scaledHorizontallyTo(0)
-			an[InvalidParameterException] should be thrownBy s.scaledVerticallyTo(0)
-			an[InvalidParameterException] should be thrownBy s.scaledToFit(1, 0)
-			an[InvalidParameterException] should be thrownBy s.scaledToFit(0, 1)
-			an[InvalidParameterException] should be thrownBy s.scaledToFit(0, 0)
-			an[InvalidParameterException] should be thrownBy s.scaledToFit(Origin)
-			an[InvalidParameterException] should be thrownBy s.scaledToCover(1, 0)
-			//			an[InvalidParameterException] should be thrownBy s.scaledToCover(0, 1)
-			//			an[InvalidParameterException] should be thrownBy s.scaledToCover(0, 0)
-			//			an[InvalidParameterException] should be thrownBy s.scaledToCover(Origin)
-
+		"scaledTo" in {
 			s.scaledTo(24, 5).size should be((24, 5): Vector)
+			s.scaledTo((24, 5): Vector).size should be((24, 5): Vector)
 			s.scaledTo(24, 20).size should be((24, 20): Vector)
+			s.scaledTo((24, 20): Vector).size should be((24, 20): Vector)
+			s.scaledTo(1, 0).size should be((1, 0): Vector)
+			s.scaledTo((1, 0): Vector).size should be((1, 0): Vector)
+			s.scaledTo(0, 1).size should be((0, 1): Vector)
+			s.scaledTo((0, 1): Vector).size should be((0, 1): Vector)
+			s.scaledTo(0, 0).size should be(Origin)
+			s.scaledTo(Origin).size should be(Origin)
 			s.scaledTo(width = 24).size should be((24, 20): Vector)
 			s.scaledTo(height = 20).size should be((24, 20): Vector)
+			an[IllegalArgumentException] should be thrownBy s.scaledTo()
 
+			S(0, 0).scaledTo(0, 0).size should be(Origin)
+			S(0, 0).scaledTo(Origin).size should be(Origin)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledTo(1, 1)
+		}
+
+		"scaledHorizontallyTo" in {
 			s.scaledHorizontallyTo(24).size should be((24, 10): Vector)
+			s.scaledHorizontallyTo(0).size should be((0, 10): Vector)
+			S(0, 0).scaledHorizontallyTo(0).size should be(Origin)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledHorizontallyTo(1)
+		}
+
+		"scaledVerticallyTo" in {
 			s.scaledVerticallyTo(20).size should be((12, 20): Vector)
+			s.scaledVerticallyTo(0).size should be((12, 0): Vector)
+			S(0, 0).scaledVerticallyTo(0).size should be(Origin)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledVerticallyTo(1)
+		}
 
+		"scaledToFit" in {
 			s.scaledToFit(24, 100).size should be((24, 20): Vector)
+			s.scaledToFit((24, 100): Vector).size should be((24, 20): Vector)
 			s.scaledToFit(120, 20).size should be((24, 20): Vector)
+			s.scaledToFit((120, 20): Vector).size should be((24, 20): Vector)
 			s.scaledToFit(6, 10).size should be((6, 5): Vector)
+			s.scaledToFit((6, 10): Vector).size should be((6, 5): Vector)
 			s.scaledToFit(12, 5).size should be((6, 5): Vector)
+			s.scaledToFit((12, 5): Vector).size should be((6, 5): Vector)
+			s.scaledToFit(12, 0).size should be(Origin)
+			s.scaledToFit((12, 0): Vector).size should be(Origin)
+			s.scaledToFit(0, 10).size should be(Origin)
+			s.scaledToFit((0, 10): Vector).size should be(Origin)
+			s.scaledToFit(0, 0).size should be(Origin)
+			s.scaledToFit(Origin).size should be(Origin)
+			S(0, 0).scaledToFit(0, 0).size should be(Origin)
+			S(0, 0).scaledToFit(Origin).size should be(Origin)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToFit(0, 1)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToFit((0, 1): Vector)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToFit(1, 0)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToFit((1, 0): Vector)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToFit(1, 1)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToFit((1, 1): Vector)
+		}
 
+		"scaledToCover" in {
 			s.scaledToCover(24, 100).size should be((120, 100): Vector)
+			s.scaledToCover((24, 100): Vector).size should be((120, 100): Vector)
 			s.scaledToCover(120, 20).size should be((120, 100): Vector)
+			s.scaledToCover((120, 20): Vector).size should be((120, 100): Vector)
 			s.scaledToCover(6, 10).size should be((12, 10): Vector)
+			s.scaledToCover((6, 10): Vector).size should be((12, 10): Vector)
 			s.scaledToCover(12, 5).size should be((12, 10): Vector)
+			s.scaledToCover((12, 5): Vector).size should be((12, 10): Vector)
 			s.scaledToCover(6, 5).size should be((6, 5): Vector)
+			s.scaledToCover((6, 5): Vector).size should be((6, 5): Vector)
+			s.scaledToCover(24, 0).size should be(Origin)
+			s.scaledToCover((24, 0): Vector).size should be(Origin)
+			s.scaledToCover(0, 20).size should be(Origin)
+			s.scaledToCover((0, 20): Vector).size should be(Origin)
+			s.scaledToCover(0, 0).size should be(Origin)
+			s.scaledToCover(Origin).size should be(Origin)
+			S(0, 0).scaledToCover(0, 0).size should be(Origin)
+			S(0, 0).scaledToCover(Origin).size should be(Origin)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToCover(0, 1)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToCover((0, 1): Vector)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToCover(1, 0)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToCover((1, 0): Vector)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToCover(1, 1)
+			an[IllegalArgumentException] should be thrownBy S(0, 0).scaledToCover((1, 1): Vector)
 		}
 	}
-
 }
 
 case class S(var x: Double, var y: Double) extends Scalable[S] {
 	def size = (x, y)
 
-	def scaledBy(x: Double, y: Double) = if (x == 0 || y == 0) throw new InvalidParameterException else S(x * this.x, y * this.y)
+	def scaledBy(x: Double, y: Double) = S(x * this.x, y * this.y)
 
 	def scaleBy(x: Double, y: Double) {
-		if (x == 0 || y == 0) throw new InvalidParameterException else {
-			this.x *= x
-			this.y *= y
-		}
+		this.x *= x
+		this.y *= y
 	}
 }
