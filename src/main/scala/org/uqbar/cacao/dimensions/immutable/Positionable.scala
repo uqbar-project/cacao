f408044343abaffc974528f5ab5609c9364fd614
Nicolas <Nicolas Scarcella>
2014-10-18 19:35:41 -0300

    Rotable and Positionable immutable implementations

--- src/main/scala/org/uqbar/cacao/dimensions/immutable/Positionable.scala
+++ src/main/scala/org/uqbar/cacao/dimensions/immutable/Positionable.scala
@@ -1,5 +1,22 @@
 package org.uqbar.cacao.dimensions.immutable
 
-trait Positionable {
+import org.uqbar.math.vectors._
+import scala.Double.NaN
 
+trait Positionable[T <: Positionable[T]] { self: T =>
+
+	def position: Vector
+
+	def moved(x: Double, y: Double): T
+	def moved(delta: Vector): T = moved(delta.x, delta.y)
+
+	def movedTo(position: Vector): T = moved(position - this.position)
+
+	def movedToAlign(x: T ⇒ Double, y: T ⇒ Double)(target: Vector): T = movedToAlign(x(this), y(this))(target)
+	def movedToAlign(what: Vector)(target: Vector): T = movedToAlign(what.x, what.y)(target)
+	def movedToAlign(x: Double = NaN, y: Double = NaN)(target: Vector): T = {
+		require(!x.isNaN || !y.isNaN, "At least one argument must be supplied.")
+
+		moved(if (x.isNaN) 0 else target.x - x, if (y.isNaN) 0 else target.y - y)
+	}
 }
--- src/main/scala/org/uqbar/cacao/dimensions/immutable/Rotable.scala
+++ src/main/scala/org/uqbar/cacao/dimensions/immutable/Rotable.scala
@@ -1,5 +1,12 @@
 package org.uqbar.cacao.dimensions.immutable
 
-trait Rotable {
+import org.uqbar.math.vectors._
 
+trait Rotable[T <: Rotable[T]] { self: T =>
+
+	def center: Vector
+
+	def rotated(radians: Double, center: Vector = this.center): T
+
+	def rotatedToPoint(point: Vector, center: Vector = this.center)(target: Vector): T = rotated(center - point angleTo (center - target))
 }
