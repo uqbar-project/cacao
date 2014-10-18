40d8a60eb22c0272200ee0453bdb6a427fec0ce2
Nicolas <Nicolas Scarcella>
2014-10-18 10:11:09 -0300

    Added many Shape stubs

--- src/main/scala/org/uqbar/cacao/Shape.scala
+++ src/main/scala/org/uqbar/cacao/Shape.scala
@@ -5,5 +5,10 @@ import org.uqbar.math.vectors.Vector
 trait Shape
 
 case class Line(from: Vector, to: Vector) extends Shape
+case class Rect(m: Double, b: Double) extends Shape
 case class Rectangle(from: Vector, size: Vector) extends Shape
 case class Circle(at: Vector, radius: Double) extends Shape
+case class RegularPoligon(sides: Int) extends Shape
+case class Poligon(corners: Vector*) extends Shape
+case class BesierCurve() extends Shape
+case class ComposedShape(shapes: Shape*) extends Shape
